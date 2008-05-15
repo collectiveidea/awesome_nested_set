0f6587399fc58b178d3a02b48bd0d60677524836
Jeff Dean <jeff@zilkey.com>
2008-05-14 23:31:43 -0400

    make the view helpers a little more railish

--- lib/awesome_nested_set_helper.rb
+++ lib/awesome_nested_set_helper.rb
@@ -1,6 +1,6 @@
 module CollectiveIdea
   module Acts #:nodoc:
-    
+
     # This module provides some helpers for the model classes using acts_as_nested_set.
     # It is included by default in all views. If you need to remove it, edit the last line
     # of init.rb.
@@ -23,20 +23,19 @@ module CollectiveIdea
       def nested_set_options_for_select(class_or_item)
         class_or_item = class_or_item.roots if class_or_item.is_a?(Class)
         items = Array(class_or_item)
-        result = []
-        items.each {|i| result += i.self_and_descendants.map {|i| [yield(i), i.id] } }
-        result
+        returning [] do |result|
+          items.each {|i| result += i.self_and_descendants.map {|i| [yield(i), i.id] } }
+        end
       end  
-      
-      
+
       # This variation of nested_set_options_for_select takes a mover node and won't show
       # any nodes that the mover can't move to.
       def nested_set_options_for_select_without_impossible_moves(class_or_item, mover)
         class_or_item = class_or_item.roots if class_or_item.is_a?(Class)
         items = Array(class_or_item)
-        result = []
-        items.each {|i| result += i.self_and_descendants.map {|i| [yield(i), i.id] if mover.new_record? || mover.move_possible?(i)}.compact }
-        result
+        returning [] do |result|
+          items.each {|i| result << i.self_and_descendants.map {|i| [yield(i), i.id] if mover.new_record? || mover.move_possible?(i)}.compact }
+        end
       end
     end
   end  
