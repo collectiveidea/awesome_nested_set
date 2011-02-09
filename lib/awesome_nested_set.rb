5c5e93291c70d0b4884bafc0feb7367f0d1145dc
Tom Garbutt <tomgarbutt@gmail.com>
2011-02-09 16:23:24 +0000

    Fixing up for rails3

--- lib/awesome_nested_set.rb
+++ lib/awesome_nested_set.rb
@@ -86,8 +86,8 @@ module CollectiveIdea #:nodoc:
               end_eval
             end
           
-            named_scope :roots, :conditions => {parent_column_name => nil}, :order => quoted_left_column_name
-            named_scope :leaves, :conditions => "#{quoted_right_column_name} - #{quoted_left_column_name} = 1", :order => quoted_left_column_name
+            scope :roots, where(parent_column_name => nil).order(quoted_left_column_name)
+            scope :leaves, where("#{quoted_right_column_name} - #{quoted_left_column_name} = 1").order(quoted_left_column_name)
 
             define_callbacks("before_move", "after_move")
           end
@@ -564,7 +564,7 @@ module CollectiveIdea #:nodoc:
                 "WHEN #{self.class.base_class.primary_key} = :id THEN :new_parent " +
                 "ELSE #{quoted_parent_column_name} END",
               {:a => a, :b => b, :c => c, :d => d, :id => self.id, :new_parent => new_parent}
-            ], nested_set_scope.proxy_options[:conditions])
+            ])
           end
           target.reload_nested_set if target
           self.reload_nested_set
