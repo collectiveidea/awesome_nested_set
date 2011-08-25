4f7c7b3ca96df4cd91e5104f8f7a215c51985ae4
Rahmat Budiharso <rbudiharso@gmail.com>
2011-08-25 13:31:20 +0700

    modified helper to allow using array of objects as argument

--- spec/awesome_nested_set/helper_spec.rb
+++ spec/awesome_nested_set/helper_spec.rb
@@ -8,7 +8,7 @@ describe "Helper" do
   end
 
   describe "nested_set_options" do
-    def test_nested_set_options
+    it "test_nested_set_options" do
       expected = [
         [" Top Level", 1],
         ["- Child 1", 2],
@@ -23,7 +23,7 @@ describe "Helper" do
       actual.should == expected
     end
 
-    def test_nested_set_options_with_mover
+    it "test_nested_set_options_with_mover" do
       expected = [
         [" Top Level", 1],
         ["- Child 1", 2],
@@ -31,6 +31,34 @@ describe "Helper" do
         [" Top Level 2", 6]
       ]
       actual = nested_set_options(Category, categories(:child_2)) do |c|
+        "#{'-' * c.level} #{c.name}"
+      end
+      actual.should == expected
+    end
+
+    it "test_nested_set_options_with_array_as_argument_without_mover" do
+      expected = [
+        [" Top Level", 1],
+        ["- Child 1", 2],
+        ['- Child 2', 3],
+        ['-- Child 2.1', 4],
+        ['- Child 3', 5],
+        [" Top Level 2", 6]
+      ]
+      actual = nested_set_options(Category.all) do |c|
+        "#{'-' * c.level} #{c.name}"
+      end
+      actual.should == expected
+    end
+
+    it "test_nested_set_options_with_array_as_argument_with_mover" do
+      expected = [
+        [" Top Level", 1],
+        ["- Child 1", 2],
+        ['- Child 3', 5],
+        [" Top Level 2", 6]
+      ]
+      actual = nested_set_options(Category.all, categories(:child_2)) do |c|
         "#{'-' * c.level} #{c.name}"
       end
       actual.should == expected
