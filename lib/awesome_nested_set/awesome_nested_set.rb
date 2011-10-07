f957fa687d037f4e9badd4afdd1c9e2f3da4d6c0
Michael Deering <mdeering@mdeering.com>
2011-10-07 11:41:04 -0600

    Added in the association callbacks to the children :has_many association

--- lib/awesome_nested_set/awesome_nested_set.rb
+++ lib/awesome_nested_set/awesome_nested_set.rb
@@ -64,7 +64,11 @@ module CollectiveIdea #:nodoc:
           :inverse_of => :children
         has_many :children, :class_name => self.base_class.to_s,
           :foreign_key => parent_column_name, :order => quoted_left_column_name,
-          :inverse_of => :parent
+          :inverse_of => :parent,
+          :before_add    => options[:before_add]    || nil,
+          :after_add     => options[:after_add]     || nil,
+          :before_remove => options[:before_remove] || nil,
+          :after_remove  => options[:after_remove]  || nil
 
         attr_accessor :skip_before_destroy
 
