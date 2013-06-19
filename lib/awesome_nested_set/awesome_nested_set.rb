9ccb826eda9a3bf1fe4064272bebfe99c3383e40
Ivan Zotov <ivanzotov@gmail.com>
2013-06-19 13:12:32 +0400

    Fix #connection deprecation warnings

--- lib/awesome_nested_set/awesome_nested_set.rb
+++ lib/awesome_nested_set/awesome_nested_set.rb
@@ -729,23 +729,23 @@ module CollectiveIdea #:nodoc:
         end
 
         def quoted_left_column_name
-          connection.quote_column_name(left_column_name)
+          self.class.connection.quote_column_name(left_column_name)
         end
 
         def quoted_right_column_name
-          connection.quote_column_name(right_column_name)
+          self.class.connection.quote_column_name(right_column_name)
         end
 
         def quoted_depth_column_name
-          connection.quote_column_name(depth_column_name)
+          self.class.connection.quote_column_name(depth_column_name)
         end
 
         def quoted_parent_column_name
-          connection.quote_column_name(parent_column_name)
+          self.class.connection.quote_column_name(parent_column_name)
         end
 
         def quoted_scope_column_names
-          scope_column_names.collect {|column_name| connection.quote_column_name(column_name) }
+          scope_column_names.collect {|column_name| self.class.connection.quote_column_name(column_name) }
         end
 
         def quoted_left_column_full_name
