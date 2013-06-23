3a6b4ccc40a646ef73ff07531d00b18ad3870782
Shevaun Coker <shevaun.coker@gmail.com>
2013-06-23 11:32:16 +1000

    pull out pruning logic into its own module and split up complex method

--- lib/awesome_nested_set/model.rb
+++ lib/awesome_nested_set/model.rb
@@ -1,3 +1,5 @@
+require 'awesome_nested_set/model/prunable'
+
 module CollectiveIdea #:nodoc:
   module Acts #:nodoc:
     module NestedSet #:nodoc:
@@ -7,6 +9,7 @@ module CollectiveIdea #:nodoc:
 
         included do
           delegate :quoted_table_name, :to => self
+          include Prunable
         end
 
         module ClassMethods
@@ -469,40 +472,6 @@ module CollectiveIdea #:nodoc:
             logger.info "Deadlock detected on retry #{retry_count}, restarting transaction"
             sleep(rand(retry_count)*0.1) # Aloha protocol
             retry
-          end
-        end
-
-        # Prunes a branch off of the tree, shifting all of the elements on the right
-        # back to the left so the counts still work.
-        def destroy_descendants
-          return if right.nil? || left.nil? || skip_before_destroy
-
-          in_tenacious_transaction do
-            reload_nested_set
-            # select the rows in the model that extend past the deletion point and apply a lock
-            nested_set_scope.right_of(left).select(id).lock(true)
-
-            if acts_as_nested_set_options[:dependent] == :destroy
-              descendants.each do |model|
-                model.skip_before_destroy = true
-                model.destroy
-              end
-            else
-              descendants.delete_all
-            end
-
-            # update lefts and rights for remaining nodes
-            diff = right - left + 1
-            nested_set_scope.where(["#{quoted_left_column_full_name} > ?", right]).update_all(
-                                                                                              ["#{quoted_left_column_name} = (#{quoted_left_column_name} - ?)", diff]
-                                                                                              )
-
-            nested_set_scope.where(["#{quoted_right_column_full_name} > ?", right]).update_all(
-                                                                                               ["#{quoted_right_column_name} = (#{quoted_right_column_name} - ?)", diff]
-                                                                                               )
-
-            # Don't allow multiple calls to destroy to corrupt the set
-            self.skip_before_destroy = true
           end
         end
 
