3a0efcb6ebf0b0859396a655050d9a95d9ce8c94
Philip Arndt <p@arndt.io>
2013-06-27 06:07:10 +1200

    Extracted validator predicates to Validators module.

--- lib/awesome_nested_set/model.rb
+++ lib/awesome_nested_set/model.rb
@@ -1,9 +1,9 @@
 require 'awesome_nested_set/model/prunable'
 require 'awesome_nested_set/model/movable'
 require 'awesome_nested_set/model/transactable'
+require 'awesome_nested_set/model/validators'
 require 'awesome_nested_set/tree'
 require 'awesome_nested_set/iterator'
-require 'awesome_nested_set/set_validator'
 
 module CollectiveIdea #:nodoc:
   module Acts #:nodoc:
@@ -14,6 +14,7 @@ module CollectiveIdea #:nodoc:
 
         included do
           delegate :quoted_table_name, :to => self
+          extend Validators
           include Prunable
           include Movable
           include Transactable
@@ -37,64 +38,12 @@ module CollectiveIdea #:nodoc:
             where("#{quoted_right_column_full_name} - #{quoted_left_column_full_name} = 1").order(quoted_left_column_full_name)
           end
 
-          def valid?
-            left_and_rights_valid? && no_duplicates_for_columns? && all_roots_valid?
-          end
-
           def left_of_right_side(node)
             where(["#{quoted_right_column_full_name} <= ?", node])
           end
 
           def right_of(node)
             where(["#{quoted_left_column_full_name} >= ?", node])
-          end
-
-          def left_and_rights_valid?
-            SetValidator.new(self).valid?
-          end
-
-          def no_duplicates_for_columns?
-            scope_string = Array(acts_as_nested_set_options[:scope]).map do |c|
-              connection.quote_column_name(c)
-            end.push(nil).join(", ")
-            [quoted_left_column_full_name, quoted_right_column_full_name].all? do |column|
-              # No duplicates
-              select("#{scope_string}#{column}, COUNT(#{column})").
-                group("#{scope_string}#{column}").
-                having("COUNT(#{column}) > 1").
-                first.nil?
-            end
-          end
-
-          # Wrapper for each_root_valid? that can deal with scope.
-          def all_roots_valid?
-            if acts_as_nested_set_options[:scope]
-              all_roots_valid_by_scope?(roots)
-            else
-              each_root_valid?(roots)
-            end
-          end
-
-          def all_roots_valid_by_scope?(roots_to_validate)
-            roots_grouped_by_scope(roots_to_validate).all? do |scope, grouped_roots|
-              each_root_valid?(grouped_roots)
-            end
-          end
-
-          def roots_grouped_by_scope(roots_to_group)
-            roots_to_group.group_by {|record|
-              scope_column_names.collect {|col| record.send(col.to_sym) }
-            }
-          end
-
-          def each_root_valid?(roots_to_validate)
-            left = right = 0
-            roots_to_validate.all? do |root|
-              (root.left > left && root.right > right).tap do
-                left = root.left
-                right = root.right
-              end
-            end
           end
 
           # Rebuilds the left & rights if unset or invalid.
@@ -351,9 +300,9 @@ module CollectiveIdea #:nodoc:
         # reload left, right, and parent
         def reload_nested_set
           reload(
-                 :select => "#{quoted_left_column_full_name}, #{quoted_right_column_full_name}, #{quoted_parent_column_full_name}",
-                 :lock => true
-                 )
+            :select => "#{quoted_left_column_full_name}, #{quoted_right_column_full_name}, #{quoted_parent_column_full_name}",
+            :lock => true
+          )
         end
 
         def reload_target(target)
