215d3f707a2b7955cdcb9707e27a6fbfdb970d16
Damian Legawiec <damian@sparksolutions.co>
2019-07-31 15:34:13 +0200

    Rename `order_column` method to `order_column_name`
    To avoid name clashing with Rails itself. Fixes https://github.com/collectiveidea/awesome_nested_set/issues/407

--- lib/awesome_nested_set/model.rb
+++ lib/awesome_nested_set/model.rb
@@ -79,7 +79,7 @@ module CollectiveIdea #:nodoc:
           end
 
           def nested_set_scope(options = {})
-            options = {:order => { order_column => :asc }}.merge(options)
+            options = {:order => { order_column_name => :asc }}.merge(options)
 
             where(options[:conditions]).order(options.delete(:order))
           end
