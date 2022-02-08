6409bab837c9fa786509616bc409422b47ddbece
Philip Arndt <git@p.arndt.io>
2022-02-09 10:34:59 +1300

    Update library version

--- lib/awesome_nested_set/version.rb
+++ lib/awesome_nested_set/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module AwesomeNestedSet
-  VERSION = '3.4.0' unless defined?(::AwesomeNestedSet::VERSION)
+  VERSION = '3.5.0' unless defined?(::AwesomeNestedSet::VERSION)
 end
